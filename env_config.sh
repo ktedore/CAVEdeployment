095bd9ba2860afe88604fc042e060b74f6264220
ktedore <ktedore@users.noreply.github.com>
2025-02-27 19:52:44 +0000

    update env config for devcontainer

--- env_config.sh
+++ env_config.sh
@@ -1,3 +1,3 @@
 # Set the path to your private environments repo
-export ENV_REPO_PATH="/Users/ktedore/Documents/Development/CAVEdeployment/environments"
+export ENV_REPO_PATH="/environments-custom" #"/Users/ktedore/Documents/Development/CAVEdeployment/environments"
 
