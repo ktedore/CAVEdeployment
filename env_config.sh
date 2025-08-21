d9b83f4d3d00d8e7a66a331e83cdb12082115b87
ktedore <ktedore@users.noreply.github.com>
2025-08-21 08:59:45 +0000

    update environment variable location

--- env_config.sh
+++ env_config.sh
@@ -1,3 +1,3 @@
 # Set the path to your private environments repo
-export ENV_REPO_PATH="/environments-custom" #"/Users/ktedore/Documents/Development/CAVEdeployment/environments"
+export ENV_REPO_PATH="/environments-custom/environments" #"/Users/ktedore/Documents/Development/CAVEdeployment/environments"
 
